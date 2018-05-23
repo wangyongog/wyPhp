18f7d63612dfa04bdddd61f4fa3c9b26e12a851e
leku008 <qq370562046@163.com>
2018-05-23 16:32:23 +0800

    message

---
+++ wap/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/1
+ * Time: 14:01
+ */
+error_reporting(E_ALL);
+include '../framework/APPbase.php';
+APPbase::run();
+//WyPhp\Database\DB::fetch_all();
