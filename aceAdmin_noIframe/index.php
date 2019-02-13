fd51a7bcc69721141cc7c6be64af392dcd9fc68e
leku008 <qq370562046@163.com>
2019-02-13 16:55:50 +0800

    t

--- aceAdmin_noIframe/index.php
+++ aceAdmin_noIframe/index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/7/1
- * Time: 14:01
- */
 //error_reporting(E_ALL ^ E_NOTICE);
 include '../framework/APPbase.php';
 class APP extends APPbase{
