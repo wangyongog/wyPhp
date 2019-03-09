5cae0aa66f1b2792c5a686be9635a29bdb379cc2
leku008 <qq370562046@163.com>
2019-03-09 14:31:24 +0800

    y

--- Common/functions.php
+++ Common/functions.php
@@ -342,7 +342,7 @@ function isMobile($mobile) {
     if (!is_numeric($mobile)) {
         return false;
     }
-    return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
+    return preg_match("/^1[3456789]{1}\d{9}$/",$mobile);
 }
 /**
  * 生成唯一订单号
