267121ff87ef8e5a1f58b75e63b64f4bac6b0e17
leku008 <qq370562046@163.com>
2019-07-29 17:40:10 +0800

    +

--- framework/APPbase.php
+++ framework/APPbase.php
@@ -50,6 +50,7 @@ class APPbase{
     public static function _initApp(){
         $url = parse_url($_SERVER['REQUEST_URI']);
         if(empty($url)){
+            
             \WyPhp\Error::http_status();
         }
         $url = trim($url['path'], '/');
